a785e218e284a7e9d8f81df31c2cd8f37ca2cc04
franksun-11 <s2564099@ed.ac.uk>
2025-10-28 00:16:23 +0000

    JSON结构化输出

--- src/main/java/com/example/aicodehelper/ai/AiCodeHelperService.java
+++ src/main/java/com/example/aicodehelper/ai/AiCodeHelperService.java
@@ -4,7 +4,15 @@ import dev.langchain4j.service.MemoryId;
 import dev.langchain4j.service.SystemMessage;
 import dev.langchain4j.service.UserMessage;
 
+import java.util.List;
+
 public interface AiCodeHelperService {
     @SystemMessage("fromResource = 'system-prompt.txt'")
     String chat(@MemoryId int memoryId, @UserMessage String userMessage);
+
+    @SystemMessage("fromResource = 'system-prompt.txt'")
+    Report chatForReport(@MemoryId int memoryId, @UserMessage String userMessage);
+
+    // 学习报告
+    record Report(String name, List<String> suggestionList){};
 }
--- src/test/java/com/example/aicodehelper/ai/AiCodeHelperServiceTest.java
+++ src/test/java/com/example/aicodehelper/ai/AiCodeHelperServiceTest.java
@@ -23,7 +23,14 @@ class AiCodeHelperServiceTest {
         result = aiCodeHelperService.chat(1, "Hi, who am i?");
         System.out.println(result);
         // 不同用户的独立会话
-        String result3 = aiCodeHelperService.chat(2, "我叫什么名字?");
+        String result3 = aiCodeHelperService.chat(2, "Hi, who am i?");
         System.out.println(result3);  // "我不知道你叫什么"
     }
-}  
+
+    @Test
+    void chatForReport() {
+        String userMessage = "你好，请帮我生成一份关于人工智能发展的报告";
+        AiCodeHelperService.Report report = aiCodeHelperService.chatForReport(1, userMessage);
+        System.out.println(report);
+    }
+}
